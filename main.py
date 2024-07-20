228ae2b62f608e6fc98875a7ffa172f4f89f7208
radhitmus <radhityajavash@gmail.com>
2024-07-20 17:28:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+karakter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+panjang_karakter = input("masukkan panjang sandi:")
+password = ""
+for i in range(int(panjang_karakter)):
+    password += random.choice(karakter)
+
+print(password)
+
+acak = random.randint(1, 10)
